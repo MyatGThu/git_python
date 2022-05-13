9980c900a64d936d9f14b161304748a4af066445
Myat Thu <104615040+MyatGThu@users.noreply.github.com>
2022-05-13 23:25:00 +1000

    Update comm_applications.py
    creating histogram code in use with dictionaries for counting things in a dictionaries.

--- comm_applications.py
+++ comm_applications.py
@@ -1,1 +1,23 @@
 # dictionaries common applications
+# in operator to look up in dictionaries
+
+
+#histogram code
+
+counts = dict()
+names = ['csev', 'cwen', 'csev', 'rqian', 'cwen', 'cwen']
+
+for name in names:
+    if name not in counts:
+        counts[name] = 1
+    else:
+        counts[name] = counts.get(name,0) + 1
+print(counts)
+
+#get method for dictionaries
+#if name in counts:
+#    x = counts[name]
+#else:
+#    x = 0
+
+# x = counts.get(name,0)
