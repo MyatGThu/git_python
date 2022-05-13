f2814308ad76545504c7ab562cd69ec3fc7f6d68
Myat Thu <104615040+MyatGThu@users.noreply.github.com>
2022-05-14 00:21:34 +1000

    Update ex9.py

--- exercises/ex9.py
+++ exercises/ex9.py
@@ -9,15 +9,21 @@ d = dict()
 
 for line in handle:
     line = line.rstrip()
-    # print(line)
     wds = line.split()
-    # print(wds)
     for w in wds:
-        print('**', w, d.get(w,-99))
-        if w in d:
-            d[w] = d[w] + 1
-        else:
-            d[w] = 1
-        # print(w, d[w])
+        #idiom retrieve/create/update counter
+        d[w] = d.get(w,0) + 1
+        print(w, 'new', d[w])
+# print(d)
 
-print(d)
+#find the most common word in the dict
+
+largest = -1
+
+for x,y in d.items(): #x and y = key and value inside dict()
+    print(x,y)
+    if y > largest:
+        largest = y
+        theword = x #capture the largest key
+
+print('Done', largest, theword)
